517e5ab76f170e6b40218b4fdd2b05cc92de7749
Prajwal Bati <prajwalbati@gmail.com>
2018-08-15 15:00:58 +0545

    Tasks list complete

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -16,6 +16,7 @@ window.Vue = require('vue');
  */
 
 Vue.component('example', require('./components/Example.vue'));
+Vue.component('task-list', require('./components/TaskList.vue'));
 
 const app = new Vue({
     el: '#app'
